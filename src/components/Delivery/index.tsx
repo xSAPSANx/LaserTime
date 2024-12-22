978608277eed7c5105cc237c5c68e27cd5792b3d
xSAPSANx <misterdinal2000@mail.ru>
2024-12-22 22:07:40 +0300

    fix adaptive and text delivery block

--- src/components/Delivery/index.tsx
+++ src/components/Delivery/index.tsx
@@ -5,7 +5,7 @@ export const Delivery = () => {
 		<div className='delivery-block'>
 			<div className='delivery-content'>
 				<h2 className='delivery-title'>РАБОТАЕМ НА ВЫЕЗД</h2>
-				<p className='delivery-subtitle'>Цена договорная</p>
+				<p className='delivery-subtitle'>Выезд от 3000₽, окончательная стоимость зависит от расстояния</p>
 			</div>
 			<div className='delivery-image'>
 				<img src='/photo/carDelivery.webp' alt='Бесплатная доставка' className='delivery-van' />
