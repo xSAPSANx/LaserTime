f11a252f5b5a564947fcc3b0eaf9133c13a2e0db
xSAPSANx <misterdinal2000@mail.ru>
2024-12-24 17:48:25 +0300

    change format image

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -7,35 +7,35 @@ export const Services = () => {
 			<h2 className='blockName'>Примеры работ</h2>
 			<ShowPhoto
 				title={'Промышленное оборудование'}
-				photoOld={'/photo/cisternOld.jpg'}
-				photoNew={'/photo/cisternNew.jpg'}
+				photoOld={'/photo/cisternOld.webp'}
+				photoNew={'/photo/cisternNew.webp'}
 				altOld={'Большой резервуар с загрязнениями'}
 				altNew={'Большой резервуар отчищенный до состояния как новый'}
 			/>
 			<ShowPhoto
 				title={'Антиквариат'}
-				photoOld={'/photo/cupOld.jpeg'}
-				photoNew={'/photo/cupNew.jpeg'}
+				photoOld={'/photo/cupOld.webp'}
+				photoNew={'/photo/cupNew.webp'}
 				altOld={'Старая загрязненная чаша с крышкой'}
 				altNew={'Очищенная старая чаша с крышкой'}
 			/>
 			<ShowPhoto
 				title={'Детали автомобиля'}
-				photoOld={'/photo/engineOld.jpeg'}
-				photoNew={'/photo/engineNew.jpeg'}
+				photoOld={'/photo/engineOld.webp'}
+				photoNew={'/photo/engineNew.webp'}
 				altOld={'Часть двигателя в моторном масле'}
 				altNew={'Часть двигателя очищенная от масла'}
 			/>
 			<ShowPhoto
-				photoOld={'/photo/crugOld.jpeg'}
-				photoNew={'/photo/crugNew.jpeg'}
+				photoOld={'/photo/crugOld.webp'}
+				photoNew={'/photo/crugNew.webp'}
 				altOld={'Запчасть машины вся в масле '}
 				altNew={'Очищенная до блеска запчасть машины'}
 			/>
 			<ShowPhoto
 				title={'Инструменты'}
-				photoOld={'/photo/knifeOld.jpeg'}
-				photoNew={'/photo/knifeNew.jpeg'}
+				photoOld={'/photo/knifeOld.webp'}
+				photoNew={'/photo/knifeNew.webp'}
 				altOld={'Лезвие ножа с ржавчиной'}
 				altNew={'Лезвие ножа очищенное от ржавчины'}
 			/>
